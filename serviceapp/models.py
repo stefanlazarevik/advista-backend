f2e7cab426c1fb7630a99942eff4e565dab95f69
alexkozminski <102160228+alexkozminski@users.noreply.github.com>
2022-10-21 17:05:02 +0600

    add models for tiktok api

--- serviceapp/models.py
+++ serviceapp/models.py
@@ -38,3 +38,72 @@ class Users(AbstractUser):
 
     class Meta:
         db_table = "users"
+
+
+class TiktokInfo(models.Model):
+    bc_id = models.CharField(max_length=100, unique=True)
+    app_id = models.CharField(max_length=100)
+    secret = models.CharField(max_length=255)
+    access_token = models.CharField(max_length=255)
+    bc_info = models.JSONField(default=dict)
+
+    class Meta:
+        db_table = "tiktok_info"
+
+
+class Advertisers(models.Model):
+    advertiser_id = models.CharField(max_length=100, unique=True)
+    role = models.CharField(max_length=100, null=True)
+    display_timezone = models.CharField(max_length=100, null=True)
+    description = models.CharField(max_length=100, null=True)
+    promotion_center_city = models.CharField(max_length=100, null=True)
+    license_url = models.CharField(max_length=100, null=True)
+    contacter = models.CharField(max_length=100, null=True)
+    owner_bc_id = models.CharField(max_length=100, null=True)
+    promotion_area = models.CharField(max_length=100, null=True)
+    license_city = models.CharField(max_length=100, null=True)
+    license_no = models.CharField(max_length=100, null=True)
+    currency = models.CharField(max_length=100, null=True)
+    cellphone_number = models.CharField(max_length=100, null=True)
+    telephone_number = models.CharField(max_length=100, null=True)
+    email = models.CharField(max_length=100, null=True)
+    promotion_center_province = models.CharField(max_length=100, null=True)
+    company = models.CharField(max_length=255, null=True)
+    language = models.CharField(max_length=100, null=True)
+    name = models.CharField(max_length=100, null=True)
+    status = models.CharField(max_length=100, null=True)
+    balance = models.CharField(max_length=100, null=True)
+    country = models.CharField(max_length=100, null=True)
+    timezone = models.CharField(max_length=100, null=True)
+    license_province = models.CharField(max_length=100, null=True)
+    advertiser_account_type = models.CharField(max_length=100, null=True)
+    brand = models.CharField(max_length=100, null=True)
+    industry = models.CharField(max_length=100, null=True)
+    rejection_reason = models.CharField(max_length=100, null=True)
+    create_time = models.BigIntegerField()
+    address = models.TextField(null=True)
+
+    class Meta:
+        db_table = "advertisers"
+
+
+class Reports(models.Model):
+    advertiser_id = models.ForeignKey(Advertisers, on_delete=models.SET_NULL, null=True, to_field='advertiser_id', db_column='advertiser_id')
+    real_time_conversion = models.CharField(max_length=50, null=True)
+    cost_per_conversion = models.CharField(max_length=50, null=True)
+    cpm = models.CharField(max_length=50, null=True)
+    clicks = models.CharField(max_length=50, null=True)
+    ctr = models.CharField(max_length=50, null=True)
+    conversion = models.CharField(max_length=50, null=True)
+    skan_conversion_rate = models.CharField(max_length=50, null=True)
+    skan_conversion = models.CharField(max_length=50, null=True)
+    conversion_rate = models.CharField(max_length=50, null=True)
+    real_time_cost_per_conversion = models.CharField(max_length=50, null=True)
+    skan_cost_per_conversion = models.CharField(max_length=50, null=True)
+    impressions = models.CharField(max_length=50, null=True)
+    spend = models.CharField(max_length=50, null=True)
+    cpc = models.CharField(max_length=50, null=True)
+    report_date = models.DateField()
+
+    class Meta:
+        db_table = "reports"
