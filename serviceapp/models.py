09704e5a69d47cded856d16e14bd1766b364826c
alexkozminski <102160228+alexkozminski@users.noreply.github.com>
2022-12-12 13:12:02 +0600

    update models

--- serviceapp/models.py
+++ serviceapp/models.py
@@ -12,6 +12,14 @@ def get_vertical_details():
     return {'name': "", 'category': "", 'domains': [], 'stats': [], 'source': [], 'url': []}
 
 
+def get_creative_video():
+    return [{'id': "", 'url': "", 'filename': "", 'size': 0, 'source': [], 'type': ""}]
+
+
+def get_create_pixel():
+    return {'label': "", 'url': ""}
+
+
 def get_domain_for():
     return {'id': "", 'email': "", 'name': ""}
 
@@ -195,6 +203,12 @@ class CampaignReports(models.Model):
     keyword = models.TextField(null=True)
     adtitle = models.TextField(null=True)
     device = models.CharField(max_length=255, null=True)
+    subid1 = models.CharField(max_length=100, null=True)
+    subid2 = models.CharField(max_length=100, null=True)
+    subid3 = models.CharField(max_length=100, null=True)
+    subid4 = models.CharField(max_length=100, null=True)
+    network = models.CharField(max_length=100, null=True)
+    site = models.CharField(max_length=100, null=True)
 
     class Meta:
         db_table = "campaign_report"
@@ -204,6 +218,7 @@ class MediaBuyer(models.Model):
     media_buyer_id = models.CharField(max_length=100, unique=True)
     email = models.CharField(max_length=100, null=True)
     name = models.CharField(max_length=255, null=True)
+    bc = models.JSONField(default=list, null=True)
 
     class Meta:
         db_table = "media_buyer"
@@ -222,6 +237,7 @@ class MediaBuyerAdvertiser(models.Model):
 class Vertical(models.Model):
     vertical_id = models.CharField(max_length=100, unique=True)
     details = models.JSONField(default=get_vertical_details)
+    bc = models.JSONField(default=list, null=True)
     created_time = models.DateTimeField(null=True)
 
     class Meta:
@@ -247,6 +263,27 @@ class Domains(models.Model):
     source = models.CharField(max_length=100, null=True)
     stats = models.CharField(max_length=100, null=True)
     pixel_id = models.CharField(max_length=255, null=True)
+    vertical = models.JSONField(default=list, null=True)
+    country = models.CharField(max_length=100, null=True)
+    creative_text = models.TextField(null=True)
+    creative_video = models.JSONField(default=get_creative_video, null=True)
+    language = models.CharField(max_length=100, null=True)
+    network = models.JSONField(default=list, null=True)
+    tracker_url = models.TextField(null=True)
+    ticket_no = models.CharField(max_length=100, null=True)
+    tiktok_account = models.JSONField(default=list, null=True)
+    type = models.CharField(max_length=100, null=True)
+    request_id = models.CharField(max_length=255, null=True)
+    category = models.JSONField(default=list, null=True)
+    create_pixel = models.JSONField(default=get_create_pixel, null=True)
+    account_id = models.JSONField(default=list, null=True)
+    status = models.JSONField(default=list, null=True)
+    open_account = models.JSONField(default=list, null=True)
+    account_button = models.JSONField(default=get_create_pixel, null=True)
+    pixel_fire = models.JSONField(default=get_create_pixel, null=True)
+    bc = models.JSONField(default=list, null=True)
+    vertical_name = models.JSONField(default=list, null=True)
+    name = models.JSONField(default=list, null=True)
     created_time = models.DateTimeField(null=True)
 
     class Meta:
@@ -262,6 +299,20 @@ class System1Revenue(models.Model):
     clicks = models.IntegerField()
     revenue = models.FloatField(default=0.0)
     revenue_per_click = models.FloatField(default=0.0)
+    campaign = models.CharField(max_length=255, null=True)
+    total_sessions = models.IntegerField(default=0)
+    mobile_sessions = models.IntegerField(default=0)
+    desktop_sessions = models.IntegerField(default=0)
+    mobile_sessions_percentage = models.FloatField(default=0.0)
+    distinct_ip = models.IntegerField(default=0)
+    distinct_mobile_ip = models.IntegerField(default=0)
+    distinct_desktop_ip = models.IntegerField(default=0)
+    distinct_mobile_ip_percentage = models.FloatField(default=0.0)
+    searches = models.IntegerField(default=0)
+    revenue_per_session = models.FloatField(default=0.0)
+    revenue_per_search = models.FloatField(default=0.0)
+    revenue_per_ip = models.FloatField(default=0.0)
+    click_per_session_percentage = models.FloatField(default=0.0)
 
     class Meta:
         db_table = "system1_revenue"
